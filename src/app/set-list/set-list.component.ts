bdd932fbba0560764a5fedf657cf59771ee6642d
AndyGabler <andrewpgabler@gmail.com>
2024-06-08 19:21:34 -0400

    Got the REST API working for the backend. However, the primary problem I'm seeing is that I get a CORS error when I switch back to the swim set page.

--- src/app/set-list/set-list.component.ts
+++ src/app/set-list/set-list.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
-import { SwimSet, swimSets } from '../swimsets';
+import { SwimSet } from '../swimsets';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-set-list',
@@ -13,12 +15,21 @@ import { SwimSet, swimSets } from '../swimsets';
 })
 export class SetListComponent {
   constructor(
-    private router: Router
+    private router: Router,
+    public client: HttpClient
   ) {
-
+    this.swimSets = this.getSets()
   }
+  
+  swimSets: SwimSet[] = this.getSets()
 
-  swimSets = [...swimSets];
+  getSets(): SwimSet[] {
+    let apiSets: SwimSet[] = [];
+    this.client.get<SwimSet[]>("http://localhost:8080/swimsets").forEach(
+      setList => setList.forEach(set => apiSets.push(set))
+    );
+    return apiSets
+  }
 
   clickSet(swimSet: SwimSet) {
     this.router.navigate(['/setdetail'], { state: { selectedSet: swimSet } });
