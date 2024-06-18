92fb18a3e3dbe5cb75dc2cfa47e1c7763fe3d4d5
AndyGabler <andrewpgabler@gmail.com>
2024-06-17 21:09:46 -0400

    And got the amount of sets I did on a particular day working. Next steps are going to be linking to set details from the scheduled set page. Maybe styling it a bit would be useful.

--- src/app/swim-calendar/swim-calendar.component.ts
+++ src/app/swim-calendar/swim-calendar.component.ts
@@ -1,7 +1,7 @@
 
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { formatDate } from '@angular/common';
 
@@ -17,20 +17,24 @@ import { ScheduledSet } from '../scheduledset';
 })
 export class SwimCalendarComponent {
   constructor(
-    private client: HttpClient
+    private client: HttpClient,
+    private formBuilder: FormBuilder
   ) {
-    this.performLookupWithDate(this.lookupDate.value)
+    this.performLookupWithDate(this.lookupDate?.value)
   }
 
-  lookupDate = new FormControl(formatDate(new Date(), 'yyyy-MM-dd', 'en'));
+
+  lookupDate = new FormControl(formatDate(new Date(), 'yyyy-MM-dd', 'en'))
   totalYards = new FormControl(0)
+  setsPerformed = this.formBuilder.array([])
+  areasWorked = new FormControl('')
 
   changeDate(event: any) {
     let newDate = event.target.value
     this.performLookupWithDate(newDate)
   }
 
-  performLookupWithDate(date: string|null) {
+  performLookupWithDate(date: string|null|undefined) {
     console.log(date)
     if (date == null) {
       return
@@ -38,12 +42,19 @@ export class SwimCalendarComponent {
     
     let httpParams = new HttpParams().set("scheduledDate", date)
     let yardCounter = 0;
+    let labels = new Set<string>()
     this.client.get<ScheduledSet[]>("/setschedule", {params: httpParams}).subscribe((results: ScheduledSet[]) => {
+      this.setsPerformed.clear()
       results.forEach(set => {
         console.log(set)
         yardCounter += (set.scheduledSet.repCount * set.scheduledSet.repLength)
+        this.setsPerformed.push(new FormControl(set.scheduledSet.name))
+        set.scheduledSet.labels.forEach(label => labels.add(label))
       })
+      let labelList: string[] = []
+      labels.forEach(label => labelList.push(label))
       this.totalYards.setValue(yardCounter)
+      this.areasWorked.setValue(labelList.join(", "))
     })
   }
 }
